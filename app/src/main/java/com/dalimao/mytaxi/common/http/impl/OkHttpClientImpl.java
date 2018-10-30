59cc238d4f81c79606dfe8ed8b66e446094fd0aa
wuaoke <987603594@qq.com>
2018-10-30 20:58:10 +0800

    提交

---
+++ app/src/main/java/com/dalimao/mytaxi/common/http/impl/OkHttpClientImpl.java
@@ -0,0 +1,18 @@
+package com.dalimao.mytaxi.common.http.impl;
+
+import com.dalimao.mytaxi.common.http.IHttpClient;
+import com.dalimao.mytaxi.common.http.IRequest;
+import com.dalimao.mytaxi.common.http.IResponse;
+
+public class OkHttpClientImpl implements IHttpClient {
+    @Override
+    public IResponse get(IRequest request, boolean forceCache) {
+
+        return null;
+    }
+
+    @Override
+    public IResponse post(IRequest request, boolean forceCache) {
+        return null;
+    }
+}
