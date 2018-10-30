7ab64d5167f0c6742c4aa2efe7548cd7152b7e95
wuaoke <987603594@qq.com>
2018-10-30 20:46:24 +0800

    测试第二台电脑提交

--- app/src/main/java/com/dalimao/mytaxi/common/http/IHttpClient.java
+++ app/src/main/java/com/dalimao/mytaxi/common/http/IHttpClient.java
@@ -1,4 +1,7 @@
 package com.dalimao.mytaxi.common.http;
 
 public interface IHttpClient {
+    IResponse get(IRequest request, boolean forceCache);
+
+    IResponse post(IRequest request, boolean forceCache);
 }
